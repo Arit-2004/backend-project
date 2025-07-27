bc174ba03cfe7345684bf00641f611ac6f1cde79
8927950941 <aritramahattam@gmail.com>
2025-07-27 21:05:16 +0530

    connect to the cloudinary and understanding multer

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs";
+
+  // Configuration
+    cloudinary.config({ 
+        cloud_name:  process.env.CLOUDINARY_CLOUD_NAME,
+        api_key: process.env.CLOUDINARY_API_KEY ,
+        api_secret: process.env.CLOUDINARY_API_SECRET
+    });  
+
+const uploadOnCloudinary = async (localFile) =>{
+     try {
+        if (!localFile) {
+            return null;
+        }
+        const response = await cloudinary.uploader.upload(localFile,{
+            resource_type: "auto"
+        })
+        console.log("file uploaded sucessfully" , response);
+        return response;
+     } catch (error) {
+        fs.unlinkSync(localFile); // delete the file if upload fails
+        console.log("file upload failed" , error)
+        return null;
+     }
+}    
+
+export default  uploadOnCloudinary;
