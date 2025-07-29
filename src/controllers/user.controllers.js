a78e8c5a806c28db253e8d7a6e69a29474d3aa9e
8927950941 <aritramahattam@gmail.com>
2025-07-29 21:24:31 +0530

    just starting the validation

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,11 @@
 import asyncHandler from "../utils/asyncHandler.js";
 
 const userRegister = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Aritra_Mahattam"
-    });
-});
+   const {userName , email , fullname , password}=req.body 
+   console.log("email :", email)
 
+   if (!userName || !email || !fullname || !password) {
+       return res.status(400).json({message:"All fields are required"}); 
+}
+})
 export default userRegister;
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,20 @@
 import { Router } from "express";
 import userRegister from "../controllers/user.controllers.js";
+import { upload } from "../middilewares/multer.middileware.js";
 
 const router = Router();
 
-router.route("/register").post(userRegister)
+router.route("/register").post(
+    upload.fields([
+        {
+           name: "avatar",
+           maxCount: 1
+        },
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    userRegister)
 
 export default router;
