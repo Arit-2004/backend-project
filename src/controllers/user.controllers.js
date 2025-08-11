64ad02f503a547db802ac327375fe7eb8713012e
8927950941 <aritramahattam@gmail.com>
2025-08-11 20:54:18 +0530

    complete the user functions and routes

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -4,6 +4,7 @@ import { ApiError } from "../utils/apiErrors.js";
 import {uploadOnCloudinary , deleteFromCloudinary} from "../utils/cloudinary.js";
 import ApiResponse from "../utils/apiResponse.js"; // Fixed capitalization
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessTokenAndRefreshToken = async (userId) => {
   try {
@@ -166,7 +167,8 @@ return res
 
 const refreshAccessToken = asyncHandler(async (req, res) => {
   try {
-    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
+    const incomingRefreshToken =
+      req.cookies.refreshToken || req.body.refreshToken;
 
     if (!incomingRefreshToken) {
       throw new ApiError(401, "Invalid incoming refresh Token");
@@ -187,7 +189,9 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
       throw new ApiError(401, "Refresh Token is expired or invalid");
     }
 
-    const { accessToken, newrefreshToken } = await generateAccessTokenAndRefreshToken(user?._id);
+    // Corrected variable name: destructure as refreshToken
+    const { accessToken, refreshToken } =
+      await generateAccessTokenAndRefreshToken(user?._id);
 
     const options = {
       httpOnly: true,
@@ -197,39 +201,39 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     return res
       .status(200)
       .cookie("accessToken", accessToken, options)
-      .cookie("refreshToken", newrefreshToken, options)
+      .cookie("refreshToken", refreshToken, options)
       .json(
         new ApiResponse(
           200,
           {
             accessToken,
-            refreshToken: newrefreshToken
+            refreshToken
           },
           "Access Token Refreshed"
         )
       );
-
   } catch (error) {
     throw new ApiError(401, error?.message || "Invalid Refresh Token");
   }
 });
+
 
 const changeCurrentPassword = asyncHandler(async(req , res)=>{
   const {oldPassword , newPassword} = req.body
 
   const user = await User.findById(req.user?._id)
   const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
-  if (isPasswordCorrect) {
+  if (!isPasswordCorrect) {
     throw new ApiError (400 , "Inavlid Password");
   }
 
   user.password = newPassword 
-  user.save = ({validateBeforeSave : false})
+  await user.save ({validateBeforeSave : false})
 
   return res
   .status(200)
   .json(new ApiResponse(
-    200 , {} , "User fetched Sucessfully"
+    200 , {} , "Password Change Sucessfully"
   ))
 
 })
@@ -244,28 +248,32 @@ const getCurrentUser = asyncHandler(async(req , res) => {
   ))
 })
 
-const updateUserDetails = asyncHandler(async(req , res)=>{
-  const {fullname , email} = req.body
-  if(!fullname || !email){
-    throw new ApiError(400 , "Both credentails required")
+const updateUserDetails = asyncHandler(async (req, res) => {
+  const { fullname, email } = req.body;
+
+  if (!fullname || !email) {
+    throw new ApiError(400, "Both credentials are required");
   }
+
   const user = await User.findByIdAndUpdate(
     req.user?._id,
-    {$set : fullname,
-      email : email
+    {
+      $set: { fullname, email }
     },
-    {new : true}
-  ).select("-password")
-  return res 
-  .status(200)
-  .json(
-    new ApiResponse(
-      200 ,
-      user,
-      "Account updated successfully"
-    )
-  )
-})
+    { new: true }
+  ).select("-password");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        user,
+        "Account updated successfully"
+      )
+    );
+});
+
 
 
 const updateUserAvatar = asyncHandler(async (req, res) => {
@@ -336,6 +344,133 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
   );
 });
 
+const getUserChannelProfile = asyncHandler(async (req, res) => {
+  const { username } = req.params;
+
+  if (!username.trim()) {
+    throw new ApiError(400, "Username does not exist");
+  }
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        userName: username?.toLowerCase()
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscriber"
+      }
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo"
+      }
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscriber"
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo"
+        },
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscriber.subscriber"] },
+            then: true,
+            else: false
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        fullname: 1,
+        userName: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1
+      }
+    }
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exist");
+  }
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      channel[0],
+      "User account fetched successfully"
+    )
+  );
+});
+
+
+const getWatchHistory = asyncHandler(async (req, res) => {
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user._id)
+      }
+    },
+    {
+      $lookup: {
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline: [
+                {
+                  $project: {
+                    fullname: 1,
+                    username: 1,
+                    avatar: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields: {
+              owner: {
+                $first: "$owner"
+              }
+            }
+          }
+        ]
+      }
+    }
+  ]);
+
+  return res.status(200).json(
+    new ApiResponse(
+      200,
+      user[0]?.watchHistory || [],
+      "Watch history fetched successfully"
+    )
+  );
+});
+
+
 
 export { 
    userRegister,
@@ -346,5 +481,7 @@ export {
    getCurrentUser,
    updateUserDetails,
    updateUserAvatar,
-   updateUserCoverImage
+   updateUserCoverImage,
+   getUserChannelProfile,
+   getWatchHistory 
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
-import  {userRegister, loginUser, logoutUser, refreshAccessToken } from "../controllers/user.controllers.js";
+import  {userRegister, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    changeCurrentPassword,
+    getCurrentUser, 
+    updateUserDetails,
+    updateUserAvatar,
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory } from "../controllers/user.controllers.js";
 import { upload } from "../middilewares/multer.middileware.js";
 import { verifyJWT } from "../middilewares/auth.middileware.js";
 
@@ -24,5 +34,13 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT,  logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("change-password").post(verifyJWT , changeCurrentPassword)
+router.route("current-user").get(verifyJWT , getCurrentUser)
+router.route("/update-account").patch(verifyJWT , updateUserDetails)
+router.route("/avatar").patch(verifyJWT , upload.single("avatar") , updateUserAvatar)
+router.route("/coverImage").patch(verifyJWT , upload.single("coverImage") , updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT , getUserChannelProfile)
+router.route("/history").get(verifyJWT , getWatchHistory)
+
 
 export default router;
